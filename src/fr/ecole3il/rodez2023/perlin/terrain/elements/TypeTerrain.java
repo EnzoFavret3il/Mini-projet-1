c6617f8a092862ffa66dbee58c562b81dabca458
Enzo Favret <Enzo Favret@EnzoTour>
2024-02-06 20:43:28 +0100

    TypeTerrain good

---
+++ src/fr/ecole3il/rodez2023/perlin/terrain/elements/TypeTerrain.java
@@ -0,0 +1,41 @@
+package fr.ecole3il.rodez2023.perlin.terrain.elements;
+
+import java.awt.image.BufferedImage;
+/**@enum Enumeration de types de terrain avec le nom et le fichier png */
+public enum TypeTerrain {
+    COLLINES("Collines", "hills.png"),
+    DESERT("Désert", "desert.png"),
+    FORET_CONIFERES("Forêt de conifères", "coniferous_forest.png"),
+    FORET_FEUILLUS("Forêt de feuillus", "deciduous_forest.png"),
+    MARAIS("Marais", "marsh.png"),
+    MONTAGNE("Montagne", "mountain.png"),
+    OCEAN("Océan", "ocean.png"),
+    PLAINE("Plaine", "plain.png"),
+    TOUNDRA("Toundra", "tundra.png");
+
+	/**@param nom --> nom du biome
+	 * @param nomImage --> nom de l'image correpondant au biome
+	 * @param image --> type permettant de charger l'image
+	 * */
+    private final String nom;
+    private final String nomImage;
+    private BufferedImage image;
+    /**@Constructor construteur d'un type de terrain*/
+    TypeTerrain(String nom, String nomImage) {
+        this.nom = nom;
+        this.nomImage = nomImage;
+        this.image = fr.ecole3il.rodez2023.perlin.Utils.chargerTuile("data/" + nomImage);
+    }
+    /**@Getter getImage --> permet de récupérer l'image
+     * @Getter getNom --> permet de récupérer le nom du terrain*/
+    public BufferedImage getImage() {
+        return image;
+    }
+    public String getNom() {
+    	return nom;
+    }
+    @Override
+    public String toString() {
+        return "Voici le nom français du terrain: "+nom+" et voici son image associé: "+ nomImage;
+    }
+}

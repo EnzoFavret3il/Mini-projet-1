fc2c8f8f61f4c47bda9da5d03cf20891df468c22
Enzo Favret <Enzo Favret@EnzoTour>
2024-02-10 13:57:32 +0100

    GenerateurPerlin

---
+++ src/fr/ecole3il/rodez2023/perlin/terrain/generation/GenerateurPerlin.java
@@ -0,0 +1,18 @@
+package fr.ecole3il.rodez2023.perlin.terrain.generation;
+
+import fr.ecole3il.rodez2023.perlin.terrain.elements.Terrain;
+
+public class GenerateurPerlin extends GenerateurCarte{
+
+	public GenerateurPerlin(long graine) {
+		super(graine);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected Terrain genererTerrain(int i, int j, int largeur, int hauteur) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

54cc59f916605cdb5c1b516c178c4c40d2020d5a
Enzo Favret <Enzo Favret@EnzoTour>
2024-02-10 15:51:02 +0100

    gestion des valeurs et types de données javadoc getters

--- src/fr/ecole3il/rodez2023/perlin/terrain/visualisation/VisualiseurTerrain.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/visualisation/VisualiseurTerrain.java
@@ -1,6 +1,9 @@
 package fr.ecole3il.rodez2023.perlin.terrain.visualisation;
 
 import fr.ecole3il.rodez2023.perlin.terrain.elements.Carte;
+import fr.ecole3il.rodez2023.perlin.terrain.elements.Terrain;
+import fr.ecole3il.rodez2023.perlin.terrain.elements.TerrainInexistant;
+import fr.ecole3il.rodez2023.perlin.terrain.elements.TypeTerrain;
 
 public class VisualiseurTerrain {
 	/**@param --> valeur de la classe de type DetermineurTerrain, Carte*/
@@ -10,4 +13,53 @@ private Carte carte;
         this.determineurTerrain = determineurTerrain;
         this.carte = carte;
     }
+    /**@methode --> permet de retourner le type d'altitude d'un terrain */
+    public AltitudeAffichee getAltitudeAffichee(int x, int y) throws TerrainInexistant {
+        Terrain terrain = carte.getTerrain(x, y);
+        double altitude = terrain.getAltitude();
+
+        if (altitude < 0) {
+            return AltitudeAffichee.FOND_MARIN;
+        } else if (altitude < AltitudeAffichee.BASSE.getValeurLimite()) {
+            return AltitudeAffichee.BASSE;
+        } else if (altitude < AltitudeAffichee.MOYENNE.getValeurLimite()) {
+            return AltitudeAffichee.MOYENNE;
+        } else {
+            return AltitudeAffichee.ELEVEE;
+        }
+    }
+    /**@methode --> permet de retourner le type d'hydrométrie d'un terrain */
+    public HydrometrieAffichee getHydrometrieAffichee(int x, int y) throws TerrainInexistant {
+        Terrain terrain = carte.getTerrain(x, y);
+        double hydrometrie = terrain.getHydrometrie();
+
+        if (hydrometrie < HydrometrieAffichee.SEC.getValeurLimite()) {
+            return HydrometrieAffichee.SEC;
+        } else if (hydrometrie < HydrometrieAffichee.MOYEN.getValeurLimite()) {
+            return HydrometrieAffichee.MOYEN;
+        } else {
+            return HydrometrieAffichee.HUMIDE;
+        }
+    }
+    /**@methode --> permet de retourner le type de température d'un terrain */
+    public TemperatureAffichee getTemperatureAffichee(int x, int y) throws TerrainInexistant {
+        Terrain terrain = carte.getTerrain(x, y);
+        double temperature = terrain.getTemperature();
+
+        if (temperature < TemperatureAffichee.FROID.getValeurLimite()) {
+            return TemperatureAffichee.FROID;
+        } else if (temperature < TemperatureAffichee.TEMPERE.getValeurLimite()) {
+            return TemperatureAffichee.TEMPERE;
+        } else {
+            return TemperatureAffichee.CHAUD;
+        }
+    }
+    
+    /**@getTypeTerrain --> getter qui renvoie une chaine de caractère determinant le terrain ou une erreur si terrain inexistant*/
+    public String getTypeTerrain(int x, int y) throws TerrainInexistant {
+        Terrain terrain = carte.getTerrain(x, y);
+        TypeTerrain typeTerrain = determineurTerrain.determinerTerrain(terrain.getAltitude(), terrain.getHydrometrie(), terrain.getTemperature());
+        return typeTerrain.toString();
+    }
+    
 }

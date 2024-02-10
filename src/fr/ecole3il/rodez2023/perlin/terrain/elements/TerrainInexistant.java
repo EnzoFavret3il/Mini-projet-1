03ddb892fd1b76bd97eac466ab020e52db5873eb
Enzo Favret <Enzo Favret@EnzoTour>
2024-02-10 12:48:02 +0100

    Classe Terrain Inexistant

---
+++ src/fr/ecole3il/rodez2023/perlin/terrain/elements/TerrainInexistant.java
@@ -0,0 +1,11 @@
+package fr.ecole3il.rodez2023.perlin.terrain.elements;
+
+/**Classe TerrainInexistant, rien de plus à dire*/
+public class TerrainInexistant extends Exception {
+
+	public TerrainInexistant(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}

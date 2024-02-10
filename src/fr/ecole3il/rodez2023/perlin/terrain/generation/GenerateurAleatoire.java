4083bd8ffcb7a28860679772c600adc9192cb226
Enzo Favret <Enzo Favret@EnzoTour>
2024-02-10 13:16:30 +0100

    Création Générateur Aléatoire Début

---
+++ src/fr/ecole3il/rodez2023/perlin/terrain/generation/GenerateurAleatoire.java
@@ -0,0 +1,18 @@
+package fr.ecole3il.rodez2023.perlin.terrain.generation;
+
+import fr.ecole3il.rodez2023.perlin.terrain.elements.Terrain;
+
+public class GenerateurAleatoire extends GenerateurCarte{
+
+	public GenerateurAleatoire(long graine) {
+		super(graine);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected Terrain genererTerrain(int i, int j, int largeur, int hauteur) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}

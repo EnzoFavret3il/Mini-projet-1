34d10e3f6fa2015aeb8124f191a49471bd2cdeb8
Enzo Favret <Enzo Favret@EnzoTour>
2024-02-10 13:42:03 +0100

    ajout java doc + modification pifomètre avec random entre les bonnes valeurs

--- src/fr/ecole3il/rodez2023/perlin/terrain/generation/GenerateurAleatoire.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/generation/GenerateurAleatoire.java
@@ -10,14 +10,14 @@ public class GenerateurAleatoire extends GenerateurCarte{
 		super(graine);
 		// TODO Auto-generated constructor stub
 	}
-
+	/**Constructeur pour un generer un terrain de maniere aleatoire*/
 	@Override
 	protected Terrain genererTerrain(int i, int j, int largeur, int hauteur) {
 		/**@info la pif-au-métrie est dans la place*/
 		Random pifomètre = new Random();
-		double altitude= pifomètre.nextDouble();
-		double hydrometrie=pifomètre.nextDouble();
-		double temperature=pifomètre.nextDouble();
+		double altitude= pifomètre.nextDouble(0,1);//j'aurais bien aimé mettre 0 et 254 pour la ref
+		double hydrometrie=pifomètre.nextDouble(-1,1);
+		double temperature=pifomètre.nextDouble(0,1);
 		return new Terrain(altitude, hydrometrie, temperature);
 	}
 	

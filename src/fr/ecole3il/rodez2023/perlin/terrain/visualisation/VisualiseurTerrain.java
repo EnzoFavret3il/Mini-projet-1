4cdb39b70dfc8d768f8a9a26fc96ef201a889a20
Enzo Favret <Enzo Favret@EnzoTour>
2024-02-10 15:33:06 +0100

    mb

--- src/fr/ecole3il/rodez2023/perlin/terrain/visualisation/VisualiseurTerrain.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/visualisation/VisualiseurTerrain.java
@@ -3,6 +3,7 @@ package fr.ecole3il.rodez2023.perlin.terrain.visualisation;
 import fr.ecole3il.rodez2023.perlin.terrain.elements.Carte;
 
 public class VisualiseurTerrain {
+	/**@param --> valeur de la classe de type DetermineurTerrain, Carte*/
 private DetermineurTerrain determineurTerrain;
 private Carte carte;
     public VisualiseurTerrain(DetermineurTerrain determineurTerrain, Carte carte) {

f9276e1c41a8b3c9e5aaf4f380616f7934fce4d2
Enzo Favret <Enzo Favret@EnzoTour>
2024-02-10 15:55:36 +0100

    modif javadoc
    BTW  -> c'est complicado

--- src/fr/ecole3il/rodez2023/perlin/terrain/visualisation/VisualiseurTerrain.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/visualisation/VisualiseurTerrain.java
@@ -6,7 +6,7 @@ import fr.ecole3il.rodez2023.perlin.terrain.elements.TerrainInexistant;
 import fr.ecole3il.rodez2023.perlin.terrain.elements.TypeTerrain;
 
 public class VisualiseurTerrain {
-	/**@param --> valeur de la classe de type DetermineurTerrain, Carte*/
+	/**@param --> attribut de la classe de type DetermineurTerrain, Carte*/
 private DetermineurTerrain determineurTerrain;
 private Carte carte;
     public VisualiseurTerrain(DetermineurTerrain determineurTerrain, Carte carte) {

9d6f2a3512f4296d503d75ff3a6881ce9adc2085
Enzo Favret <Enzo Favret@EnzoTour>
2024-02-10 14:20:51 +0100

    OUAH Finalisation de la methode genererTerrain + javadoc

--- src/fr/ecole3il/rodez2023/perlin/terrain/generation/GenerateurPerlin.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/generation/GenerateurPerlin.java
@@ -12,11 +12,26 @@ public class GenerateurPerlin extends GenerateurCarte{
 
 	@Override
 	protected Terrain genererTerrain(int i, int j, int largeur, int hauteur) {
-		/**Generation des valeurs de types BruitPerlin2D*/
+		/**Generation des valeurs de types BruitPerlin2D
+		 * 
+		 * Vous êtes parti très très très loin dans la réalisation de cette classe
+		 * J'ai failli faire un coma
+		 * 
+		 * @BruitPerlin2D --> génération des valeurs avec des graines*/
 		BruitPerlin2D altitudePerlin = new BruitPerlin2D(getGraine(), 1); 
         BruitPerlin2D hydrometriePerlin = new BruitPerlin2D(getGraine(), 1); 
-        BruitPerlin2D temperaturePerlin = new BruitPerlin2D(getGraine() * 2, 1); 
-        
+        BruitPerlin2D temperaturePerlin = new BruitPerlin2D(getGraine() * 2, 1);
+        /**@double x/y --> création des coordonnées*/
+        double x = (double) i / (double) largeur;
+        double y = (double) j / (double) hauteur;
+        /**@double --> attribution des valeurs aux attributs*/
+        double altitude = altitudePerlin.bruit2D(x, y);
+        double hydrometrie = hydrometriePerlin.bruit2D(x, y);
+        double temperature = temperaturePerlin.bruit2D(x, y);
+        hydrometrie = (hydrometrie + 1) / 2; 
+        temperature = (temperature + 1) / 2 ; 
+        /**@return --> création d'un nouveau terrain*/
+        return new Terrain(altitude, hydrometrie, temperature);
 	}
 
 }

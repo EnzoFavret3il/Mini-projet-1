666a45887337bca027a5561be2f4f6d98e1f9dfe
Enzo Favret <Enzo Favret@EnzoTour>
2024-02-10 14:59:11 +0100

    Enum AltitudeAffichee avec gestion des vals

---
+++ src/fr/ecole3il/rodez2023/perlin/terrain/visualisation/AltitudeAffichee.java
@@ -0,0 +1,19 @@
+package fr.ecole3il.rodez2023.perlin.terrain.visualisation;
+
+public enum AltitudeAffichee {
+	BASSE(0.33),
+    MOYENNE(0.66),
+    ELEVEE(1.0),
+    FOND_MARIN(-1.0);
+
+    private final double valeurLimite;
+
+    AltitudeAffichee(double valeurLimite) {
+        this.valeurLimite = valeurLimite;
+    }
+
+    public double getValeurLimite() {
+        return valeurLimite;
+        
+    }
+}

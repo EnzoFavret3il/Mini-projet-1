d61850db616868acfd0f720f3dd8f00b2e45326a
Enzo Favret <151365706+EnzoFavret3il@users.noreply.github.com>
2024-02-20 18:47:36 +0100

    Ajout JavaDoc

--- src/fr/ecole3il/rodez2023/perlin/terrain/concrets/VisualiseurTerrainEnonce.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/concrets/VisualiseurTerrainEnonce.java
@@ -3,6 +3,8 @@ package fr.ecole3il.rodez2023.perlin.terrain.concrets;
 import fr.ecole3il.rodez2023.perlin.terrain.elements.Carte;
 import fr.ecole3il.rodez2023.perlin.terrain.visualisation.VisualiseurTerrain;
 import fr.ecole3il.rodez2023.perlin.terrain.elements.TypeTerrain;
+
+//Contructeur héritage du visualisateur de terrain ennonce
 public class VisualiseurTerrainEnonce extends VisualiseurTerrain {
 	public VisualiseurTerrainEnonce(Carte c) {
 		super(new DetermineurTerrainEnonce(), c);

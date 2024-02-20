cba8ae25082f2c0d86623f20a32fedb6b23a888c
Enzo Favret <151365706+EnzoFavret3il@users.noreply.github.com>
2024-02-20 18:46:06 +0100

    Ajout JavaDoc

--- src/fr/ecole3il/rodez2023/perlin/terrain/visualisation/DetermineurTerrain.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/visualisation/DetermineurTerrain.java
@@ -1,9 +1,9 @@
 package fr.ecole3il.rodez2023.perlin.terrain.visualisation;
 
 import fr.ecole3il.rodez2023.perlin.terrain.elements.TypeTerrain;
-
+//Interface pour le DetermineurTerrain
 public interface DetermineurTerrain {
-
+	//DeterminerTerrain en fonction de ses caractéristiques
 	public TypeTerrain determinerTerrain(double altitude, double hydrometrie, double temperature);
 
 

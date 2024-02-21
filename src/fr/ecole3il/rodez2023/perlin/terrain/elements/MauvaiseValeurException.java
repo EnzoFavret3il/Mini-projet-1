5fa94ac8247e29434e417cc7ce1df65354e9af47
EnzoFavret3IL <favrete@3il.fr>
2024-02-21 20:19:41 +0100

    Modification JavaDoc

--- src/fr/ecole3il/rodez2023/perlin/terrain/elements/MauvaiseValeurException.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/elements/MauvaiseValeurException.java
@@ -1,6 +1,6 @@
 package fr.ecole3il.rodez2023.perlin.terrain.elements;
 
-/**@constructor --> constructor d'une exception pour gérer els mauvaises valeurs des données du terrain*/
+/**@constructor --> constructor d'une exception pour gérer les mauvaises valeurs des données du terrain*/
 public class MauvaiseValeurException extends IllegalArgumentException {
 	public MauvaiseValeurException(String message) {
 		super(message);
